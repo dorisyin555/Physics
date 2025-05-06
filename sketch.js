dedf651b53b248f8b0299b4eaed33aab62adfd7e
dorisyin555 <Doris_Yin@avenues-ny.org>
2025-05-06 09:53:30 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,92 +1,41 @@
 let colorlist = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000']
 
-let movers = []
-let G = 0.1
-let wind 
+let mover
+let G
+let jumpVector 
+let leftVector
+let rightVector
 
 function setup() {
   createCanvas(400, 400);
-  for( let i = 0; i < 10; i++ ) {
-    movers.push( 
-      new Mover(random(width),random(height),random(-1,1),random(-1,1),10,random(colorlist))
-    )  
-  }
-  wind = random(-.2,.2)
+  mover = Mover.createStandardMoverAt(10,height/2)
+  G = createVector(0,2)
+  jumpVector = createVector(0,-70)
+  leftVector = createVector(-3,0)
+  rightVector = createVector(3,0)
   ellipseMode(RADIUS)
 }
 
 function draw() {
   background(220);
-  for( let mover of movers ) {
-    mover.update()
-  }
-  
+  mover.applyForce(G)
+  mover.update()
 }
 
-class Mover { // noun
-  // properties - adjectives
-  //   radius
-  //   velocity (dx,dy)
-  //   position (x,y)
-  //   color
-  // behaviors - verb
-  //   move
-  //   draw
-  //   collide/bounce
-  
-  // initialize all the properties
-  constructor(x,y,dx,dy,r,c) {
-    this.x = x
-    this.y = y
-    this.dx = dx
-    this.dy = dy
-    this.r = r
-    this.c = c
+function keyPressed() {
+  // jump
+  if( key === " " ) {
+    // apply a force
+    // negative y value
+    mover.applyForce(jumpVector)
   }
   
-  update() {
-    this.applyGravity()
-    this.applyWind()
-    this.move()
-    this.containWithinWindow()
-    this.draw()     
+  if( keyCode === LEFT_ARROW ) {
+    mover.applyForce(leftVector)
+  }
+
+  if( keyCode === RIGHT_ARROW ) {
+    mover.applyForce(rightVector)
   }
   
-  draw() {
-    fill(this.c)
-    circle(this.x,this.y,this.r)
-  }
-  
-  move() {
-    this.x += this.dx
-    this.y += this.dy
-  }
-  
-  applyGravity() {
-    this.dy += G
-  }
-  
-  applyWind() {
-    this.dx += wind
-  }
-  
-  containWithinWindow() {
-    if( this.x < this.r ) { // moved off the left hand side
-      this.x = this.r
-      this.dx *= -1
-    }
-    if( this.x > width - this.r ) { // right
-      this.x = width-this.r
-      this.dx *= -1
-    }
-    if( this.y < this.r ) { // top
-      this.y = this.r
-      this.dy *= -1
-    }
-    if( this.y > height - this.r ) { // bottom
-      this.y = height-this.r
-      this.dy *= -1
-    }    
-      
-  }
 }
